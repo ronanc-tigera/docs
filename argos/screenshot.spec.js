b636ef909cc3301d267c29effdc5fed0ae8334ed
Ronan Conneely <ronan.conneely@tigera.io>
2024-01-08 08:43:09 +0000

    for of loop

--- argos/screenshot.spec.js
+++ argos/screenshot.spec.js
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
-import {test} from '@playwright/test';
+import {test, expect} from '@playwright/test';
 import {argosScreenshot} from '@argos-ci/playwright';
 import {extractSitemapPathnames, pathnameToArgosName} from './utils';
 
@@ -15,6 +15,22 @@ const stylesheet = fs.readFileSync(stylesheetPath).toString();
 function waitForDocusaurusHydration() {
   return document.documentElement.dataset.hasHydrated === 'true';
 }
+
+
+// function cleanPathname (pathname) {
+//   let converted = pathname.replaceAll('/', '_');
+
+//   if (converted.charAt(0) === '_') {
+//     converted = converted.substring(1, converted.length);
+//   }
+
+//   if (converted.charAt(converted.length - 1) === '_') {
+//     converted = converted.substring(0, converted.length -1);
+//   }
+
+//   return converted;
+// }
+
 
 function screenshotPathname(pathname) {
   test(`pathname ${pathname}`, async ({page}) => {
@@ -32,6 +48,32 @@ test.describe('Docusaurus site screenshots', () => {
   pathnames = pathnames.slice(2, 3);
   console.log("Pathnames to screenshot:", pathnames);
   pathnames.forEach(screenshotPathname);
+  for (const path of pathnames) {
+    screenshotPathname(path);
+  }
   // test 1 first
   // screenshotPathname('/calico-cloud/about/');
 });
+
+
+// function screenshotPathname(pathname) {
+//   test(`pathname ${pathname}`, async ({page}) => {
+//     const url = siteUrl + pathname;
+//     console.log("URL = ", url);
+//     await page.goto(url);
+//     await page.waitForFunction(waitForDocusaurusHydration);
+//     await page.addStyleTag({content: stylesheet});
+//     // await argosScreenshot(page, pathnameToArgosName(pathname));
+//     await expect(page).toHaveScreenshot({fullPage: true});
+//   });
+// }
+
+// test.describe('Docusaurus site screenshots', () => {
+//   let pathnames = extractSitemapPathnames(sitemapPath);
+//   pathnames = pathnames.slice(2, 3);
+//   console.log("Pathnames to screenshot:", pathnames);
+//   ///////// try for loop!!!!!!!!!!!!!!!!
+//   pathnames.forEach(screenshotPathname);
+//   // test 1 first
+//   // screenshotPathname('/calico-cloud/about/');
+// });
