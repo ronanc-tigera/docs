64a5b221f6f881078d066212131bfb7de338c59c
Ronan Conneely <ronan.conneely@tigera.io>
2024-09-24 12:05:07 +0100

    extractPathname.js

---
+++ __screenshot-tests__/extractPathnames.js
@@ -0,0 +1,20 @@
+const cheerio = require('cheerio');
+const fs = require('fs');
+
+const sitemapPath = './build/sitemap.xml';
+
+// Extract a list of pathnames, given a fs path to a sitemap.xml file
+const extractSitemapPathnames = (sitemapPath) => {
+  const sitemap = fs.readFileSync(sitemapPath).toString();
+  const $ = cheerio.load(sitemap, { xmlMode: true });
+  const urls = [];
+  $('loc').each(function handleLoc() {
+    urls.push($(this).text());
+  });
+  return urls.map((url) => new URL(url).pathname);
+};
+
+const extractPathnames = () =>
+  fs.writeFileSync('__screenshot-tests__/pathnames.json', JSON.stringify(extractSitemapPathnames(sitemapPath), null, 2));
+
+extractPathnames();
