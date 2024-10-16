b6ea8449dcc4e6febdd44e806e8f20fd7da5923f
Christopher Tauchen <chris.tauchen@tigera.io>
2024-10-16 16:49:56 +0100

    Updates to Felix configuration mechanism

--- src/___new___/components/FelixConfig/index.js
+++ src/___new___/components/FelixConfig/index.js
@@ -1,69 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import jsonData from './file.json';
+import FelixConfigTable from '../FelixConfigTable';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 
-const FelixConfig = ({ name }) => {
-  const [group, setGroup] = useState(null);
 
-  useEffect(() => {
-    // Find the group that matches the passed 'name' prop
-    const matchedGroup = jsonData.Groups.find(
-      (group) => group.Name === name
+const FelixConfig = ({ configType, name }) => {
+  const matchedGroup = jsonData.Groups.find(group => group.Name === name);
+  // Debugging logs
+
+  if (!matchedGroup) {
+    return <p>No matching group found for '{name}'.</p>;
+  } 
+
+
+  let content;
+
+  if (configType === "yaml") {
+    content = (
+      <div>
+        {matchedGroup.Fields.map((field, index) => (
+          <div key={index}>
+            <h5><code>{field.NameYAML}</code></h5>
+            <FelixConfigTable configType="yaml" fieldData={field} />
+          </div>
+        ))}
+      </div>
     );
 
-    // Set the matched group to state
-    if (matchedGroup) {
-      setGroup(matchedGroup);
-    } else {
-      setGroup(null);
-    }
-  }, [name]);
-
-  return (
-    <div>
-      {group ? (
-        Array.isArray(group.Fields) && group.Fields.length > 0 ? (
-          group.Fields.map((field, index) => (
-            <div key={index}>
-              <h5 id={`${field.NameYAML}`}><code>{field.NameYAML}</code></h5>
-              <p dangerouslySetInnerHTML={{ __html: field.DescriptionHTML }} />
-              <table>
-                <thead>
-                <tr>
-                  <th>Description</th>
-                  <th>Value</th>
-                </tr>
-                </thead>
-                <tbody>
-                <tr>
-                  <td>Schema</td>
-                  <td dangerouslySetInnerHTML={{ __html: field.YAMLSchemaHTML }} />
-                </tr>
-                <tr>
-                  <td>Default</td>
-                  <td>
-                    {field.StringDefault === '' ? (
-                      'none'
-                    ) : field.GoType === '*v1.Duration' ? (
-                      <>
-                        <code>{field.StringDefault}</code> (<code>{field.ParsedDefault}</code>)
-                      </>
-                    ) : (
-                      <code>{field.StringDefault}</code>
-                    )}
-                  </td>
-                </tr>
-                </tbody>
-              </table>
-            </div>
-          ))
-        ) : (
-          <p>No fields found in the group.</p>
-        )
-      ) : (
-        <p>No matching group found for '{name}'.</p>
-      )}
-    </div>
-  );
-};
+  } else if (configType === "configenv") {
+    content = (
+      <div>
+        {matchedGroup.Fields.map((field, index) => (
+          <div key={index}>
+            <h4><code>{field.NameConfigFile}</code></h4>
+        <Tabs groupId="operating-systems">
+          <TabItem value="apple" label="Configuration file">
+             <FelixConfigTable configType='config' fieldData={field}/>
+          </TabItem>
+          <TabItem value="orange" label="Environment variable">
+            <FelixConfigTable configType='env' fieldData={field}/>
+          </TabItem>
+        </Tabs>
+        </div>
+        ))}
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        <p>You got it wrong</p>
+      </div>
+    );
+  };
+    return (
+      <div>
+        {content}
+      </div>
+    );
+  };
 
 export default FelixConfig;
--- src/___new___/components/FelixConfigTable/index.js
+++ src/___new___/components/FelixConfigTable/index.js
@@ -1,87 +1,138 @@
-import Tabs from '@theme/Tabs';
-import TabItem from '@theme/TabItem';
 import React from 'react';
-// Importing the actual JSON data
-import configData from '../FelixConfig/file.json';
 
-function FelixConfigTable({ type, field }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Attribute</th>
-          <th>Value</th>
-        </tr>
-      </thead>
-      <tbody>
-        {type === 'YAML' ? (
-          <>
-            <tr>
-              <td>Key name</td>
-              <td>{field.NameYAML}</td>
-            </tr>
-            <tr>
-              <td>Description</td>
-              <td dangerouslySetInnerHTML={{ __html: field.DescriptionHTML }} />
-            </tr>
-            <tr>
-              <td>Schema</td>
-              <td dangerouslySetInnerHTML={{ __html: field.YAMLSchemaHTML }} />
-            </tr>
-            <tr>
-              <td>Default</td>
-              <td>{field.YAMLDefault}</td>
-            </tr>
-          </>
-        ) : type === 'ConfigEnv' ? (
-          <>
-            <tr>
-              <td>Key name</td>
-              <td>{field.NameConfigFile}</td>
-            </tr>
-            <tr>
-              <td>Environment Variable</td>
-              <td>{field.NameEnvVar}</td>
-            </tr>
-            <tr>
-              <td>Description</td>
-              <td dangerouslySetInnerHTML={{ __html: field.DescriptionHTML }} />
-            </tr>
-            <tr>
-              <td>Schema</td>
-              <td dangerouslySetInnerHTML={{ __html: field.StringSchemaHTML }} />
-            </tr>
-            <tr>
-              <td>Default</td>
-              <td>{field.StringDefault}</td>
-            </tr>
-          </>
-        ) : (
-          <tr>
-            <td colSpan="2">Invalid type</td>
-          </tr>
-        )}
-      </tbody>
-    </table>
-  );
-}
+const TableCRD = ({ fieldData }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Attribute</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+      <td>Key</td>
+      <td><code>{fieldData.NameYAML || 'No Default Value'}</code></td>
+      </tr>
+      <tr>
+        <td>Description</td>
+        <td dangerouslySetInnerHTML={{ __html: fieldData.DescriptionHTML }} />
+      </tr>
+      <tr>
+        <td>Schema</td>
+        <td dangerouslySetInnerHTML={{ __html: fieldData.YAMLSchemaHTML }} />
+      </tr>
+      <tr>
+        <td>Default</td>
+        <td>
+                    {fieldData.YAMLDefault === '' ? (
+                      'none'
+                    ) : (
+                      <code>{fieldData.YAMLDefault}</code>
+                    )}
+                  </td>
+      </tr>
+    </tbody>
+  </table>
+);
 
-function FelixConfig({ type, group }) {
-  // Find the group by name from the JSON data
-  const selectedGroup = configData.Groups.find(g => g.Name === group);
+const TableConfig = ({ fieldData }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>AttributeConfig</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+      <td>Key</td>
+      <td><code>{fieldData.NameConfigFile || 'No Default Value'}</code></td>
+      </tr>
+      <tr>
+        <td>Description</td>
+        <td dangerouslySetInnerHTML={{ __html: fieldData.DescriptionHTML }} />
+      </tr>
+      <tr>
+        <td>Schema</td>
+        <td dangerouslySetInnerHTML={{ __html: fieldData.StringSchemaHTML }} />
+      </tr>
+      <tr>
+        <td>Default</td>
+        <td>
+                    {fieldData.YAMLDefault === '' ? (
+                      'none'
+                    ) : (
+                      <code>{fieldData.YAMLDefault}</code>
+                    )}
+                  </td>
+      </tr>
+    </tbody>
+  </table>
+);
 
-  if (!selectedGroup) {
-    return <p>Group not found</p>;
+const TableEnv = ({ fieldData }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>AttributeEnv</th>
+        <th>Value</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+      <td>Key</td>
+      <td><code>{fieldData.NameEnvVar.toUpperCase() || 'No Default Value'}</code></td>
+      </tr>
+      <tr>
+        <td>Description</td>
+        <td dangerouslySetInnerHTML={{ __html: fieldData.DescriptionHTML }} />
+      </tr>
+      <tr>
+        <td>Schema</td>
+        <td dangerouslySetInnerHTML={{ __html: fieldData.StringSchemaHTML }} />
+      </tr>
+      <tr>
+        <td>Default</td>
+        <td>
+                    {fieldData.StringDefault === '' ? (
+                      'none'
+                    ) : fieldData.GoType === '*v1.Duration' ? (
+                      <>
+                        <code>{fieldData.StringDefault}</code> ({fieldData.ParsedDefault})
+                      </>
+                    ): (
+                      <code>{fieldData.StringDefault}</code>
+                    )}
+                  </td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+function FelixConfigTable({ configType, fieldData }) {
+  let table;
+
+
+  // Determine which table to render based on the configType
+  switch (configType) {
+    case 'yaml':
+      table = <TableCRD fieldData={fieldData} />;
+      break;
+    case 'config':
+      table = <TableConfig fieldData={fieldData} />;
+      break;
+    case 'env':
+      table = <TableEnv fieldData={fieldData} />;
+      break;
+    default:
+      table = <p>Unknown config type</p>;
   }
 
   return (
-    <>
-      <h2>Group: {group} ({type})</h2>
-      {selectedGroup.Fields.map((field, index) => (
-        <FelixConfigTable key={index} type={type} field={field} />
-      ))}
-    </>
+    <div>
+      {table}
+    </div>
   );
 }
 
-export default FelixConfig;
+export default FelixConfigTable;
