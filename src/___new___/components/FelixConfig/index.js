618d72eb148f5560f33fca208fa6e1a247f27534
Christopher Tauchen <chris.tauchen@tigera.io>
2024-10-17 14:59:22 +0100

    Changes from review

--- src/___new___/components/FelixConfig/index.js
+++ src/___new___/components/FelixConfig/index.js
@@ -1,48 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import jsonData from './file.json';
-import FelixConfigTable from '../FelixConfigTable';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
+import TableCRD from './TableCRD';
+import TableConfig from './TableConfig';
+import TableEnv from './TableEnv';
+import Styles from './styles.css';
 
+const FelixConfigTable = ({ configType, fieldData }) => {
+  let table;
+
+  switch (configType) {
+    case 'yaml':
+      table = <TableCRD fieldData={fieldData} />;
+      break;
+    case 'config':
+      table = <TableConfig fieldData={fieldData} />;
+      break;
+    case 'env':
+      table = <TableEnv fieldData={fieldData} />;
+      break;
+    default:
+      table = <p>Unknown config type</p>;
+  }
+
+  return <div>{table}</div>;
+};
 
 const FelixConfig = ({ configType, name }) => {
-  const matchedGroup = jsonData.Groups.find(group => group.Name === name);
+  const matchedGroup = jsonData.Groups.find((group) => group.Name === name);
   // Debugging logs
 
   if (!matchedGroup) {
     return <p>No matching group found for '{name}'.</p>;
-  } 
-
+  }
 
   let content;
 
-  if (configType === "yaml") {
+  if (configType === 'yaml') {
     content = (
       <div>
         {matchedGroup.Fields.map((field, index) => (
           <div key={index}>
-            <h5><code>{field.NameYAML}</code></h5>
-            <FelixConfigTable configType="yaml" fieldData={field} />
+            <h4>
+              <code>{field.NameYAML}</code>
+            </h4>
+            <FelixConfigTable
+              configType='yaml'
+              fieldData={field}
+            />
           </div>
         ))}
       </div>
     );
-
-  } else if (configType === "configenv") {
+  } else if (configType === 'configenv') {
     content = (
       <div>
         {matchedGroup.Fields.map((field, index) => (
           <div key={index}>
-            <h4><code>{field.NameConfigFile}</code></h4>
-        <Tabs groupId="operating-systems">
-          <TabItem value="apple" label="Configuration file">
-             <FelixConfigTable configType='config' fieldData={field}/>
-          </TabItem>
-          <TabItem value="orange" label="Environment variable">
-            <FelixConfigTable configType='env' fieldData={field}/>
-          </TabItem>
-        </Tabs>
-        </div>
+            <Tabs groupId='operating-systems'>
+              <TabItem
+                value='apple'
+                label='Configuration file'
+              >
+                <FelixConfigTable
+                  configType='config'
+                  fieldData={field}
+                />
+              </TabItem>
+              <TabItem
+                value='orange'
+                label='Environment variable'
+              >
+                <FelixConfigTable
+                  configType='env'
+                  fieldData={field}
+                />
+              </TabItem>
+            </Tabs>
+          </div>
         ))}
       </div>
     );
@@ -52,12 +88,8 @@ const FelixConfig = ({ configType, name }) => {
         <p>You got it wrong</p>
       </div>
     );
-  };
-    return (
-      <div>
-        {content}
-      </div>
-    );
-  };
+  }
+  return <div>{content}</div>;
+};
 
 export default FelixConfig;
